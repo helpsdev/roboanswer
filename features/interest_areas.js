057aa38a5f9274ed579b123ad0e3616adf3d67ad
Edwin <eperez@prokarma.com>
2020-10-21 22:21:25 -0600

    Hear for all the main sections of resume.json. Give personalized bot reply when navigating in an array-like section of resume.json

--- features/interest_areas.js
+++ features/interest_areas.js
@@ -3,15 +3,41 @@ const MyResume = require('./resume.json');
 module.exports = function (controller) {
     
     controller.hears(Object.keys(MyResume),"message", async (bot, message) => {
-        await bot.reply(message, {
-            text: "I think you're interested in one of these would you help me pointing in the rigth direction?",
-            quick_replies: Object.keys(MyResume[message.text]).map(e => {
-                return {
-                    title: e,
-                    payload: e
-                };
-            })
-        });
+        const areaOfInterestObj = MyResume[message.text];
+
+        if (Array.isArray(areaOfInterestObj)) {
+            
+            let detailResponse;
+            switch (message.text) {
+                case "work":
+                    detailResponse = "places I've worked on: ";
+                    break;
+                case "volunteer":
+                    detailResponse = "volunteer I've been part of: ";
+                    break;
+                default:
+                    break;
+            }
+
+            await bot.reply(message, "This are the " + detailResponse);
+            for (elem of areaOfInterestObj){
+                const firstPropName = Object.keys(elem)[0];
+                await bot.reply(message, `${firstPropName}: ${elem[firstPropName]}`);
+            }
+
+        }else{
+            await bot.reply(message, {
+                text: "I think you're interested in one of these would you help me pointing in the rigth direction?",
+                quick_replies: Object.keys(areaOfInterestObj).map(toQuickReply)
+            });
+        }
     });
 
+}
+
+function toQuickReply(propName){
+    return{
+        title: propName,
+        payload: propName
+    };
 }
